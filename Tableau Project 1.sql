f4af8be3b66d30132cd9fb92f6b844b3d8c6f7da
Sabbir Rahman <116674419+Sabbirrah@users.noreply.github.com>
2023-02-16 00:54:03 +0000

    Update Tableau Project 1.sql
    This project will include the usage of SSMS, Excel and Tableau

--- Tableau Project 1.sql
+++ Tableau Project 1.sql
@@ -1,12 +1,7 @@
-/*
-
-Queries used for Tableau Project
-
-*/
+-- Queries used for Tableau Project
 
 
-
--- 1. 
+-- 1.1 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject1..CovidDeaths
@@ -27,7 +22,7 @@ order by 1,2
 --order by 1,2
 
 
--- 2. 
+-- 1.2 
 
 -- We take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
@@ -41,7 +36,7 @@ Group by location
 order by TotalDeathCount desc
 
 
--- 3.
+-- 1.3
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject1..CovidDeaths
@@ -50,7 +45,7 @@ Group by Location, Population
 order by PercentPopulationInfected desc
 
 
--- 4.
+-- 1.4
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
@@ -60,21 +55,18 @@ Group by Location, Population, date
 order by PercentPopulationInfected desc
 
 
+-- Queries used to check on data if valid to use for Tableau Project
 
 
 
 
 
 
-
-
-
-
--- Queries I originally had, but excluded some because it created too long of video
 -- Here only in case you want to check them out
+-- We will use the following queries to retreeve the interested data to export them into Excel to create tables the use the table for Tableau
 
 
--- 1.
+-- 2.1
 
 Select dea.continent, dea.location, dea.date, dea.population
 , MAX(vac.total_vaccinations) as RollingPeopleVaccinated
@@ -90,7 +82,7 @@ order by 1,2,3
 
 
 
--- 2.
+-- 2.1
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject1..CovidDeaths
 --Where location like '%states%'
@@ -111,7 +103,7 @@ order by 1,2
 --order by 1,2
 
 
--- 3.
+-- 2.3
 
 -- We take these out as they are not inluded in the above queries and want to stay consistent
 -- European Union is part of Europe
@@ -126,7 +118,7 @@ order by TotalDeathCount desc
 
 
 
--- 4.
+-- 2.4
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject1..CovidDeaths
@@ -136,7 +128,7 @@ order by PercentPopulationInfected desc
 
 
 
--- 5.
+-- 2.5
 
 --Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 --From PortfolioProject..CovidDeaths
@@ -152,7 +144,7 @@ where continent is not null
 order by 1,2
 
 
--- 6. 
+-- 2.6
 
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
@@ -172,7 +164,7 @@ Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
 From PopvsVac
 
 
--- 7. 
+-- 2.7
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject1..CovidDeaths
